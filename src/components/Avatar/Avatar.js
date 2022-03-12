594b2af022826ccb89870b669ae4e91aadac3b1c
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-03-12 10:24:34 -0300

    Modificaciones de la primer app creada

---
+++ src/components/Avatar/Avatar.js
@@ -0,0 +1,11 @@
+import './Avatar.css'
+const Avatar = () => {
+    return(
+        <picture>
+            <img src='https://randomuser.me/api/portraits/lego/5.jpg' alt='lego'></img>
+            <p>Lego</p>
+        </picture>
+    )
+}
+
+export default Avatar;
