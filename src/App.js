2fe46e13a0c23477bf8303714a527d6ec5a743f8
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-04-06 21:00:01 -0300

    Fallo masivo en merge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css';
-// Se retira importacion del componente para agregar el contenedor de items
-// import Avatar from './components/Avatar/Avatar';
-// Se tuvo que agregar import React from 'react'; en cada uno de los elementos
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Home from './components/Home/Home';
+import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import ItemListConteiner from './components/ItemListConteiner/ItemListConteiner';
-// import Contact from './components/Contact/Contact';
+import Contact from './components/Contact/Contact';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 
