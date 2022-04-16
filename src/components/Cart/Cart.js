6f71677d14aa063620e15883693ec92ba886ac1e
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2022-04-16 08:57:16 -0300

    Se agrega Header al cart, junto a sus estilos. Por otro lado se agrega el contenido de price, y suptotal price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,19 +5,31 @@ import './Cart.css';
 const Cart = () => {
     const { cart, clearCart, removeItem } = useContext(CartContext);
     return (
-        <div className='contentCart'>
-            <h1 className='contentCart-title' >Cart</h1>
-            <ul className='contentCart-list'>
-                {cart.map(prod =>
-                    <li className='contentCart-listItem' key={prod.cod}>
-                        <span>{prod.name}</span>
-                        <span>{prod.quantity}</span>
-                        <span><button className='contentCart-btnDel' onClick={()=>removeItem(prod.cod)}>x</button></span>
+        <div className='content'>
+            <div className='contentCart'>
+                <h1 className='contentCart-title' >Cart</h1>
+                <ul className='contentCart-list'>
+                    <li className='contentCart-headerItem'>
+                        <span>Producto</span>
+                        <span>Cantidad</span>
+                        <span>Unidad</span>
+                        <span>Subtotal</span>
+                        <span>Cancelar</span>
                     </li>
-                )}
-            </ul>
-            <button className='contentCart-btnClr' onClick={clearCart}>Vaciar carrito</button>
+                    {cart.map(prod =>
+                        <li className='contentCart-listItem' key={prod.cod}>
+                            <span>{prod.name}</span>
+                            <span>{prod.quantity}</span>
+                            <span>{prod.price}</span>
+                            <span>{prod.quantity*prod.price}</span>
+                            <span><button className='contentCart-btnDel' onClick={()=>removeItem(prod.cod)}>x</button></span>
+                        </li>
+                    )}
+                </ul>
+                <button className='contentCart-btnClr' onClick={clearCart}>Vaciar carrito</button>
+            </div>
         </div>
+
     )
 }
 
